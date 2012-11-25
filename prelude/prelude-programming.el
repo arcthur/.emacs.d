af3cbe12d9b34f35f09ce61126cd8c1d1a420cfd
arcthur <arthurtemptation@gmail.com>
2012-11-26 00:33:43 +0800

    enable flycheck and add flycheck-cursor

--- prelude/prelude-programming.el
+++ prelude/prelude-programming.el
@@ -1,4 +1,4 @@
-; (require 'init-latex-mode)
+(require 'init-latex-mode)
 (require 'init-html-mode)
 (require 'init-css-mode)
 (require 'init-stylus-mode)
@@ -18,7 +18,30 @@
 
 ;; Flymake
 ;; ----------------------------------------------------------
+(defun flymake-error-under-point ()
+  "Display the flymake error under the point in the minibuffer."
+  (interactive)
+  (let ((line-no (line-number-at-pos)))
+    (dolist (elem flymake-err-info)
+      (if (eq (car elem) line-no)
+        (let ((err (car (nth 1 elem))))
+        (message "%s" (flymake-ler-text err)))))))
+
+(defadvice flymake-goto-next-error (after display-message activate compile)
+  "Show the next error in the ring in the minibuffer."
+  (flymake-error-under-point))
+
+(defadvice flymake-goto-prev-error (after display-message activate compile)
+  "Show the previous error in the ring in the minibuffer."
+  (flymake-error-under-point))
+
+(defadvice flycheck-mode (before post-command-stuff activate compile)
+  "When there's an error under the point, display it in the
+minibuffer."
+  (set (make-local-variable 'post-command-hook)
+       (cons 'flymake-error-under-point post-command-hook)))
+
 ;; Enable flymake for all files
-; (add-hook 'find-file-hook 'flycheck-mode)
+(add-hook 'find-file-hook 'flycheck-mode)
 
 (provide 'prelude-programming)
