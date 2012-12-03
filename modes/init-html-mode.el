e4e7c49f8fe871882b64d073d7048787316abfbf
arcthur <arthurtemptation@gmail.com>
2012-12-03 22:24:47 +0800

    Add wrap-in-tag

--- modes/init-html-mode.el
+++ modes/init-html-mode.el
@@ -1,8 +1,3 @@
-(eval-after-load "sgml-mode"
-  '(progn
-     (define-key html-mode-map (kbd "C-<down>") 'skip-to-next-blank-line)
-     (define-key html-mode-map (kbd "C-<up>") 'skip-to-previous-blank-line)))
-
 (defun skip-to-next-blank-line ()
   (interactive)
   (let ((inhibit-changing-match-data t))
@@ -17,15 +12,29 @@
     (unless (search-backward-regexp "^\\s *$" nil t)
       (forward-char 1))))
 
+(defun html-wrap-in-tag (beg end)
+  (interactive "r")
+  (let ((oneline? (= (line-number-at-pos beg) (line-number-at-pos end))))
+    (deactivate-mark)
+    (goto-char end)
+    (unless oneline? (newline-and-indent))
+    (insert "</div>")
+    (goto-char beg)
+    (insert "<div>")
+    (unless oneline? (newline-and-indent))
+    (indent-region beg (+ end 11))
+    (goto-char (+ beg 4))))
+
+(eval-after-load "sgml-mode"
+  '(progn
+     (define-key html-mode-map (kbd "C-<down>") 'skip-to-next-blank-line)
+     (define-key html-mode-map (kbd "C-<up>") 'skip-to-previous-blank-line)
+     (define-key html-mode-map (kbd "C-c C-w") 'html-wrap-in-tag)))
+
 (autoload 'zencoding-mode "zencoding-mode")
 (autoload 'zencoding-expand-line "zencoding-mode")
 
-(add-hook 'nxml-mode-hook 'zencoding-mode)
-(add-hook 'html-mode-hook 'zencoding-mode)
-
-;; Newline after inserting closing tag in html-mode
-(defadvice sgml-close-tag (after close-tag-then-newline activate)
-  (newline-and-indent))
+(add-hook 'sgml-mode-hook 'zencoding-mode)
 
 (eval-after-load 'zencoding-mode
   '(progn
