47a86eb36d9c99fbf9cedae7bf378bd9423a6513
arcthur <arthurtemptation@gmail.com>
2013-01-13 20:54:06 +0800

    Update smartparens config

--- extensions/init-smartparens.el
+++ extensions/init-smartparens.el
@@ -6,6 +6,7 @@
 ;;; add new pairs
 (sp-add-pair "*" "*")
 (sp-add-pair "$" "$")
+(sp-add-pair "<" ">") ;; in html only!
 
 ;;; global
 (sp-add-ban-insert-pair-in-string "'")
@@ -15,38 +16,44 @@
 ;; first argument is the opening pair! Here, we want to disable ' pair
 ;; in a bunch of text modes
 (sp-with-tag "'"
-             (sp-add-local-ban-insert-pair 'markdown-mode)
-             (sp-add-local-ban-insert-pair 'tex-mode)
-             (sp-add-local-ban-insert-pair 'latex-mode)
-             (sp-add-local-ban-insert-pair 'text-mode)
-             (sp-add-local-ban-insert-pair 'log-edit-mode))
+  (sp-add-local-ban-insert-pair 'markdown-mode)
+  (sp-add-local-ban-insert-pair 'tex-mode)
+  (sp-add-local-ban-insert-pair 'latex-mode)
+  (sp-add-local-ban-insert-pair 'text-mode)
+  (sp-add-local-ban-insert-pair 'log-edit-mode)
+  (sp-add-local-ban-insert-pair 'org-mode))
 
 ;; now, we could've also done just this:
 ;; (sp-add-local-ban-insert-pair "'"
 ;;                               '(markdown-mode
-;;                                 tex-mode
-;;                                 latex-mode
-;;                                 text-mode
-;;                                 log-edit-mode))
+;;                                 ...))
 ;; but I wanted to show you how to use the sp-with-tag macro :)
 
-;;; emacs-lisp-mode
-(sp-add-local-ban-insert-pair "'" 'emacs-lisp-mode)
-(sp-add-local-ban-insert-pair "'" 'inferior-emacs-lisp-mode)
-(sp-add-local-ban-insert-pair-in-code "`" 'emacs-lisp-mode)
-(sp-add-local-ban-insert-pair-in-code "`" 'inferior-emacs-lisp-mode)
+;;; emacs-lisp-mode(s)
+(sp-with '(emacs-lisp-mode inferior-emacs-lisp-mode lisp-interaction-mode)
+  (sp-add-local-ban-insert-pair "'")
+  (sp-add-local-ban-insert-pair-in-code "`"))
 
-;; markdown-mode
-; you can also use the `sp-with' macro. It will automatically add the
-; mode to the end of each call. How cool is that!
-(sp-with 'markdown-mode
-         (sp-add-local-pair "`" "`")
-         ;; this also disables '*' in all other modes
-         (sp-add-local-allow-insert-pair "*")
-         (sp-add-tag-pair "2" "**" "**" nil))
+;;; markdown-mode
+;; you can also use the `sp-with' macro. It will automatically add the
+;; mode to the end of each call. How cool is that!
+(sp-with '(markdown-mode rst-mode)
+  (sp-add-local-pair "`" "`")
+  ;; this also disables '*' in all other modes
+  (sp-add-local-allow-insert-pair "*")
+  (sp-add-tag-pair "2" "**" "**" nil))
 
 ;;; tex-mode latex-mode
 (sp-with '(tex-mode latex-mode) ;; yes, this works with lists too!
-         (sp-add-local-allow-insert-pair "$"))
+  (sp-add-local-allow-insert-pair "$")
+  (sp-add-tag-pair "i" "\"<" "\">" nil))
+
+;;; python-mode
+(sp-with 'python-mode
+  (sp-add-local-ban-insert-pair "`"))
+
+;;; html-mode
+(sp-with '(html-mode sgml-mode)
+  (sp-add-local-allow-insert-pair "<"))
 
 (provide 'init-smartparens)
