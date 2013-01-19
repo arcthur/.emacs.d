c4e0c9105fd00a7c8f466741b8ade1fe5da332ca
arcthur <arthurtemptation@gmail.com>
2013-01-19 14:47:43 +0800

    Use new smartparens config

--- extensions/init-smartparens.el
+++ extensions/init-smartparens.el
@@ -3,57 +3,40 @@
 ;; highlights matching pairs
 (show-smartparens-global-mode t)
 
-;;; add new pairs
-(sp-add-pair "*" "*")
-(sp-add-pair "$" "$")
-(sp-add-pair "<" ">") ;; in html only!
-
-;;; global
-(sp-add-ban-insert-pair-in-string "'")
-
-;; you can also use the `sp-with-tag' macro. It will automatically add
-;; the tag to each function. Use this only with functions where the
-;; first argument is the opening pair! Here, we want to disable ' pair
-;; in a bunch of text modes
-(sp-with-tag "'"
-  (sp-add-local-ban-insert-pair 'markdown-mode)
-  (sp-add-local-ban-insert-pair 'tex-mode)
-  (sp-add-local-ban-insert-pair 'latex-mode)
-  (sp-add-local-ban-insert-pair 'text-mode)
-  (sp-add-local-ban-insert-pair 'log-edit-mode)
-  (sp-add-local-ban-insert-pair 'org-mode))
-
-;; now, we could've also done just this:
-;; (sp-add-local-ban-insert-pair "'"
-;;                               '(markdown-mode
-;;                                 ...))
-;; but I wanted to show you how to use the sp-with-tag macro :)
-
-;;; emacs-lisp-mode(s)
-(sp-with '(emacs-lisp-mode inferior-emacs-lisp-mode lisp-interaction-mode)
+;; emacs is lisp hacking enviroment, so we set up some most common
+;; lisp modes too
+(sp-with '(
+           emacs-lisp-mode
+           inferior-emacs-lisp-mode
+           lisp-interaction-mode
+           scheme-mode
+           common-lisp-mode
+           )
+  ;; disable ' everywhere, it's the quote character!
   (sp-add-local-ban-insert-pair "'")
+  ;; also disable the pseudo-quote inside code.  We keep it in
+  ;; commends and strings for hyperlinks
   (sp-add-local-ban-insert-pair-in-code "`"))
 
-;;; markdown-mode
-;; you can also use the `sp-with' macro. It will automatically add the
-;; mode to the end of each call. How cool is that!
-(sp-with '(markdown-mode rst-mode)
-  (sp-add-local-pair "`" "`")
-  ;; this also disables '*' in all other modes
-  (sp-add-local-allow-insert-pair "*")
-  (sp-add-tag-pair "2" "**" "**" nil))
+;; markdown based modes
+(sp-with '(
+           markdown-mode
+           gfm-mode
+           rst-mode
+           )
+  ;; overload the `' pair with ``, which is used for inline
+  ;; code in markdown
+  (sp-add-local-pair "`" "`"))
 
-;;; tex-mode latex-mode
-(sp-with '(tex-mode latex-mode) ;; yes, this works with lists too!
-  (sp-add-local-allow-insert-pair "$")
-  (sp-add-tag-pair "i" "\"<" "\">" nil))
-
-;;; python-mode
-(sp-with 'python-mode
-  (sp-add-local-ban-insert-pair "`"))
-
-;;; html-mode
-(sp-with '(html-mode sgml-mode)
-  (sp-add-local-allow-insert-pair "<"))
+;; LaTeX modes
+(sp-add-pair "$" "$")
+(sp-with '(
+           tex-mode
+           plain-tex-mode
+           latex-mode
+           )
+  ;; allow the dollar pair only in LaTeX related modes.  It
+  ;; often marks a variable elsewhere
+  (sp-add-local-allow-insert-pair "$"))
 
 (provide 'init-smartparens)
